1adf4389c5475d7f7e9f18bd654a9a7052e5116b
hedvigannast <hedvig.annast@gmail.com>
2025-04-06 00:29:10 +0300

    catching the fish does not work

--- Scripts/playerCharacter.gd
+++ Scripts/playerCharacter.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@onready var grab_zone = $GrabZone
+@onready var hand = $Hand
+
+var weight = false
+
+
 
 func _process(_delta):
 	var direction = Vector2(
@@ -15,3 +21,19 @@ func _process(_delta):
 	move_and_slide()
 	# Clamp position so it stays in the center third of the screen
 	#position.x = clamp(position.x, LEFT_LIMIT, RIGHT_LIMIT)
+	if Input.is_action_just_pressed("grab_fish"):
+		var areas = $GrabZone.get_overlapping_areas()
+		print("Overlapping Areas:", areas)
+		for area in areas:
+			print("Found area: ", area.name)
+
+func try_pickup():
+	for body in $GrabZone.get_overlapping_bodies():
+		if body is CharacterBody2D and body.get_parent().has_method("on_picked_up"):
+			var fish = body.get_parent()
+			if fish.can_be_picked_up():
+				fish.on_picked_up()
+				return
+
+func _on_fish_picked_up():
+	weight = true
