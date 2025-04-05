6649d5b6c84e2595f0f741c5aed8c8179ddbb61f
Grisrein <98610128+Grisrein@users.noreply.github.com>
2025-04-05 15:38:26 +0300

    Muutsin scripte

---
+++ Scripts/playerCharacter.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+# Define horizontal movement boundaries
+const LEFT_LIMIT = 427
+const RIGHT_LIMIT = 853
+
+func _process(delta):
+	var direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+
+	# Update Velocity
+	velocity = direction * move_speed
+	
+	move_and_slide()
+	# Clamp position so it stays in the center third of the screen
+	#position.x = clamp(position.x, LEFT_LIMIT, RIGHT_LIMIT)
